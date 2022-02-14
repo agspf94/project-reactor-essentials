1aae2c46e60baa4fee7fd2335b301920af4c99d6
apatriarca <apatriarca@uolinc.com>
2022-02-14 10:38:45 -0300

    Aula 01

--- src/test/kotlin/com/example/projectreactoressentials/ProjectReactorEssentialsApplicationTests.kt
+++ src/test/kotlin/com/example/projectreactoressentials/ProjectReactorEssentialsApplicationTests.kt
@@ -5,9 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ProjectReactorEssentialsApplicationTests {
-
 	@Test
-	fun contextLoads() {
+	fun test() {
+		println("Everything working as intended")
 	}
-
 }
