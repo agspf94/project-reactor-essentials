98c343fbddaa299504569724885e7bfc860fb29d
apatriarca <apatriarca@uolinc.com>
2022-02-14 10:31:00 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/projectreactoressentials/ProjectReactorEssentialsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.projectreactoressentials
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectReactorEssentialsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectReactorEssentialsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/projectreactoressentials/ProjectReactorEssentialsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.projectreactoressentials
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectReactorEssentialsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
